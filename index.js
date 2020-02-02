c748abf7a0903e6b7b8db4c235c10150ccc959a5
Puru Dahal <puru@dahal.me>
2020-02-02 00:48:53 -0800

    Initial screenshot with faster performance

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 'use strict';
 
-exports.screenshot = (request, response) => {
-  console.log('request', request)
-  response.status(401).send({
-    error: 'Unauthorized'
-  });
-  // response.status(200).send({
-  //   message: 'Glorious success'
+const puppeteer = require('puppeteer');
+let browserPromise = puppeteer.launch({ args: [ 
+  '--disable-dev-shm-usage',
+  '--disable-setuid-sandbox',
+  '--no-first-run',
+  '--no-sandbox',
+  '--no-zygote',
+  '--single-process'
+]})
+
+exports.screenshot = async (request, response) => {
+  const url       = request.query.url || 'https://google.com'
+  const browser   = await browserPromise
+  const page      = await browser.newPage()
+
+  await page.goto(url)
+
+  const image     = await page.screenshot()
+
+  response.setHeader('Content-Type', 'image/png')
+  response.send(image)
+  browser.close()
+  // response.status(401).send({
+  //   error: 'Unauthorized'
   // });
+  // // response.status(200).send({
+  // //   message: 'Glorious success'
+  // // });
 };
 
 exports.event = (event, callback) => {
