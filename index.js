3c2b0b0bdeade11235d6a1ef886808a513042aca
Puru Dahal <puru@dahal.me>
2020-02-02 01:00:36 -0800

    Using chrome-aws-lambda for better performance

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 'use strict';
 
-const puppeteer = require('puppeteer');
-let browserPromise = puppeteer.launch({ args: [ 
-  '--disable-dev-shm-usage',
-  '--disable-setuid-sandbox',
-  '--no-first-run',
-  '--no-sandbox',
-  '--no-zygote',
-  '--single-process'
-]})
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 exports.screenshot = async (request, response) => {
   const url       = request.query.url || 'https://google.com'
-  const browser   = await browserPromise
+  const browser   = await puppeteer.launch({
+                      args: chromium.args,
+                      defaultViewport: chromium.defaultViewport,
+                      executablePath: await chromium.executablePath,
+                      headless: chromium.headless,
+                    })
   const page      = await browser.newPage()
-
   await page.goto(url)
-
   const image     = await page.screenshot()
 
   response.setHeader('Content-Type', 'image/png')
